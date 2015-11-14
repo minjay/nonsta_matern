774370ce234da567aaa6f82fa073e004a6352aca
minjay <mjfan@ucar.edu>
2015-11-14 11:58:38 -0800

    load beta_hat

--- pred_nonsta_Matern.m
+++ pred_nonsta_Matern.m
@@ -1,4 +1,5 @@
 load('data_regr.mat')
+load('beta_hat.mat')
 
 rng(1)
 
@@ -12,24 +13,21 @@ m = 4;
 lambda_inv = 2.5;
 b_mat = get_nonsta_var(m, lambda_inv, theta_samples*4);
 
-beta_hat = [4.698266 1.532569 0.769803 0.832334 -0.860071 4.207303 17.918832 8.156696]; 
-%beta_hat = [3.923432 2.04541 0.396874 1.506968 -0.581043 4.839481 19.966406 9.908376]; 
-%beta_hat = [0.8937, 6.2331, -3.5112, 5.1321, 0.0806, 4.9487, 19.9509, 11.2629];
 std_vec_est = exp(b_mat*reshape(beta_hat(1:m+1), m+1, 1));
 plot(theta_samples, std_vec_est, '.')
 
 n_r = 1e3;
 r_vec = linspace(0, 2, n_r);
-value_vec = zeros(n_r, 1);
-nu = beta_hat(6);
-a = beta_hat(7);
+corr_vec = zeros(n_r, 1);
+nu = beta_hat(m+2);
+a = beta_hat(m+3);
 for i = 1:n_r
-    value_vec(i) = Matern(r_vec(i), nu, a);
+    corr_vec(i) = Matern(r_vec(i), nu, a);
 end
 % convert chordal distance to great-circle distance
 r_vec = asin(r_vec/2)*2;
 % plot correlation function
-plot(r_vec, value_vec, 'LineWidth', 2)
+plot(r_vec, corr_vec, 'LineWidth', 2)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
