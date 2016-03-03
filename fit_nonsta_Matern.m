b464e4f614bd0778b7019032983718cb46ac0bfe
minjay <mjfan@ucar.edu>
2016-03-03 11:52:19 -0800

    change it to B-spline

--- fit_nonsta_Matern.m
+++ fit_nonsta_Matern.m
@@ -1,6 +1,7 @@
 parpool(8)
 
-load('data_regr.mat')
+load('data_EOF_regr.mat')
+resid = resid_all(1, :);
 
 rng(1)
 
@@ -18,19 +19,20 @@ pot_samples = resid(index)';
 % get distance matrix
 r = get_chordal_dist(x, y, z);
 
-% non-stationary variance funcion
-m = 4;
-lambda_inv = 2.5;
-b_mat = get_nonsta_var(m, lambda_inv, theta_samples*4);
+% non-stationary variance function
+knots = [0 0 0 0 0.25 0.5 0.75 1 1 1 1]*pi;
+[b_mat, ~] = bspline_basismatrix(4, knots, theta_samples*4);
+
+m = size(b_mat, 2);
 
 % rescale the observations
 Y = pot_samples/1e3;
 
-beta_init = [zeros(1, m+1) 2 10 0.1];
+beta_init = [zeros(1, m) 2 10 0.1];
 negloglik1 = @(beta_all) negloglik_nonsta_Matern(beta_all, r, b_mat, Y);
 
-lb = [-Inf -Inf -Inf -Inf -Inf 0 0 0];
-ub = [Inf Inf Inf Inf Inf 5 Inf Inf];
+lb = [-Inf(1, m) 0 0 0];
+ub = [Inf(1, m) 5 Inf Inf];
 
 [beta_hat, f_min] = nonsta_Matern_fit(negloglik1, beta_init, lb, ub, true);
 
