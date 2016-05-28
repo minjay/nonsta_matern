23b6fba14b42698a54fd33508dc19b73b9603bd2
minjay <mjfan@ucar.edu>
2016-05-28 16:12:13 -0700

    copy

---
+++ negloglik_Gaussian_needlet.m
@@ -0,0 +1,13 @@
+function f_value = negloglik_Gaussian_needlet(beta_all, r, b_mat, samples)
+
+n = length(samples);
+beta = beta_all(1:end-1);
+tau = beta_all(end);
+
+cov_mat = get_cov_nonsta_Matern(beta, r, b_mat)+tau^2*eye(n);
+
+[beta tau]
+
+f_value = ecmnobj(reshape(samples, 1, n), zeros(n, 1), cov_mat);
+
+end
