0dc75d9b11280ec6dfa0893f70f4320d10b71a89
minjay <mjfan@ucar.edu>
2016-06-02 14:14:57 -0700

    fix eta at estimates from MCMC

--- fit_Gaussian_needlet.m
+++ fit_Gaussian_needlet.m
@@ -3,6 +3,9 @@ parpool(8)
 load('data_EOF_regr_new.mat')
 resid = resid_all(1, :);
 
+load('post_samples_real_new_resid_j24_pen_B_spline_cubic_2knots_aurora_n4000_sigma_j_sq_0.01_0.0002_nu4_long_init0.mat')
+eta_est = mean(post_samples.eta(:, 1501:end), 2);
+
 rng(1)
 
 % sampling
@@ -34,11 +37,11 @@ j_max = 4;
 
 [Npix, ~, A] = get_A_ss(B, j_min, j_max, theta_samples*4, phi_samples);
 
-beta_init = [-1 1 1 1 -1 -1 0.1 0.01 0.1];
+beta_init = [0 eta_est(2:end)' 0.1^2 0.01^2 0.1];
 negloglik1 = @(beta_all) negloglik_Gaussian_needlet(beta_all, b_mat, Y, Npix, A);
 
-lb = [-10*ones(1, m) 0 0 1e-3];
-ub = [10*ones(1, m) 1 1 Inf];
+lb = [-10 eta_est(2:end)' 0 0 1e-3];
+ub = [10 eta_est(2:end)' 1 1 Inf];
 
 [beta_hat, f_min] = Gaussian_needlet_fit(negloglik1, beta_init, lb, ub, true);
 
