fad8c5b87800b021123575f45c2a8e966cad1fa2
minjay <mjfan@ucar.edu>
2016-06-03 15:50:42 -0700

    add script to plot unconditional simulations from nonsta Matern model

---
+++ plot_nonsta_Matern.m
@@ -0,0 +1,35 @@
+load('data_EOF_regr_new.mat')
+load('beta_hat.mat')
+
+load('mat_A.mat')
+[N, M] = size(A);
+A = A(361:N-360, :);
+[N, M] = size(A);
+
+theta_vec = theta(:);
+phi_vec = phi(:);
+% stretching
+[x, y, z] = trans_coord(theta_vec*4, phi_vec);
+
+% get distance matrix
+r = get_chordal_dist(x, y, z);
+
+% non-stationary variance function
+knots = [0 0 0 0 40/180 80/180 1 1 1 1]*pi;
+[b_mat, ~] = bspline_basismatrix(4, knots, theta_vec*4);
+
+b_mat(:, 1) = 1;
+
+beta = beta_hat(1:end-1);
+tau = beta_hat(end);
+
+% get cov mat
+cov_mat = get_cov_nonsta_Matern(beta, r, b_mat)+tau^2*eye(N);
+
+figure
+T = 9;
+Y_sim_Matern = mvnrnd(zeros(T, N), cov_mat)*1000;
+for t = 1:T
+    subplot(3, 3, t)
+    plot_pot(reshape(Y_sim_Matern(t, :), size(phi)), phi, theta, 1000, max(abs(Y_sim_Matern(t, :))));
+end
