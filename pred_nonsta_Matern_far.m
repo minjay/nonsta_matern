a52b12ca5d6fb01621bbdea6ce4ca74bb0a3bf4f
minjay <mjfan@ucar.edu>
2016-06-10 17:13:24 -0600

    load precomputed cov mat

--- pred_nonsta_Matern_far.m
+++ pred_nonsta_Matern_far.m
@@ -1,75 +1,39 @@
+clear
+
 load('data_EOF_regr_new.mat')
 load('beta_hat.mat')
+% load precomputed cov mat
+load('cov_mat_Matern.mat')
 resid = resid_all(1, :);
 
 rng(1)
 
 % sampling
-n = 4*1e3;
+n = 1000;
 theta_vec = theta(:);
 phi_vec = phi(:);
 w = sin(theta_vec*4);
+w(phi_vec<=pi/2 & theta_vec<=15/180*pi) = 0;
 [pot_samples, index] = datasample(resid', n, 'Replace', false,...
     'Weights', w);
 theta_samples = theta_vec(index);
 phi_samples = phi_vec(index);
 
-% non-stationary variance function
-knots = [0 0 0 0 40/180 80/180 1 1 1 1]*pi;
-[b_mat, ~] = bspline_basismatrix(4, knots, theta_samples*4);
-
-b_mat(:, 1) = 1;
-
-m = size(b_mat, 2);
-
-std_vec_est = exp(b_mat*reshape(beta_hat(1:m), m, 1))*1e3;
-plot(theta_samples, std_vec_est, '.')
-
-n_r = 1e3;
-r_vec = linspace(0, 2, n_r);
-corr_vec = zeros(n_r, 1);
-nu = beta_hat(m+1);
-a = beta_hat(m+2);
-for i = 1:n_r
-    corr_vec(i) = Matern(r_vec(i), nu, a);
-end
-% convert chordal distance to great-circle distance
-r_vec = asin(r_vec/2)*2;
-% plot correlation function
-plot(r_vec, corr_vec, 'LineWidth', 2)
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-theta_vec = theta(:);
-phi_vec = phi(:);
-% stretching
-[x, y, z] = trans_coord(theta_vec*4, phi_vec);
-
-% total number of locations
-N = length(x);
-
-% get distance matrix
-r = get_chordal_dist(x, y, z);
-
-% non-stationary variance function
-knots = [0 0 0 0 40/180 80/180 1 1 1 1]*pi;
-[b_mat, ~] = bspline_basismatrix(4, knots, theta_vec*4);
-
-b_mat(:, 1) = 1;
-
-beta = beta_hat(1:end-1);
-tau = beta_hat(end);
-
-% get cov mat
-cov_mat = get_cov_nonsta_Matern(beta, r, b_mat)+tau^2*eye(N);
-
 % kriging
-Sigma00 = cov_mat(index, index);
+Sigma00 = cov_mat_Matern(index, index);
 tmp = Sigma00\reshape(pot_samples/1e3, n, 1);
 
-SigmaP0 = cov_mat(:, index);
-Y_pred_Matern = SigmaP0*tmp*1e3;
+SigmaP0 = cov_mat_Matern(:, index);
+Y_pred_Matern_far = SigmaP0*tmp*1e3;
 
-Y_err_Matern = resid'-Y_pred_Matern;
+Y_err_Matern_far = resid'-Y_pred_Matern_far;
 
-save('Y_pred_Matern.mat', 'Y_pred_Matern', 'Y_err_Matern')
+figure
+subplot(1, 2, 1)
+plot_pot(reshape(Y_pred_Matern_far, size(phi)), phi, theta, 1000, max(abs(Y_pred_Matern_far)))
+colormap(jet)
+subplot(1, 2, 2)
+plot_pot_with_obs(reshape(Y_err_Matern_far, size(phi)), phi, theta, phi_samples, theta_samples, 1000)
+colormap(jet)
+
+save('Y_pred_Matern_far.mat', 'Y_pred_Matern_far', 'Y_err_Matern_far')
