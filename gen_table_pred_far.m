851bb268107aafc543fdd6204bf90fdcc01d6fff
minjay <mjfan@ucar.edu>
2016-12-27 21:11:45 -0800

    Keep 3 digits.

--- gen_table_pred_far.m
+++ gen_table_pred_far.m
@@ -5,16 +5,16 @@ load('pred_Matern_far.mat')
 load('pred_Gau_needlet_far.mat')
 
 disp('MAE')
-mean([MAE_Gau_needlet MAE_Matern])
-std([MAE_Gau_needlet MAE_Matern])
+round(mean([MAE_Gau_needlet MAE_Matern]), 3)
+round(std([MAE_Gau_needlet MAE_Matern]), 3)
 
 disp('MSPE')
-mean([MSPE_Gau_needlet MSPE_Matern])
-std([MSPE_Gau_needlet MSPE_Matern])
+round(mean([MSPE_Gau_needlet MSPE_Matern]), 3)
+round(std([MSPE_Gau_needlet MSPE_Matern]), 3)
 
 disp('CRPS')
-mean([CRPS_Gau_needlet CRPS_Matern])
-std([CRPS_Gau_needlet CRPS_Matern])
+round(mean([CRPS_Gau_needlet CRPS_Matern]), 3)
+round(std([CRPS_Gau_needlet CRPS_Matern]), 3)
 
 disp('cp_50')
 round(mean([cp_50_Gau_needlet cp_50_Matern])*100, 1)
