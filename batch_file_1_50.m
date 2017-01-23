7ec3c673bf04893fccaba2e7064d7733234bd5fe
minjay <mjfan@ucar.edu>
2017-01-23 11:37:46 +0800

    Add path.

--- batch_file_1_50.m
+++ batch_file_1_50.m
@@ -1,3 +1,12 @@
+clear
+clc
+addpath(genpath('/home/minjay/NeedMat'))
+addpath(genpath('/home/minjay/overcomplete'))
+addpath(genpath('/home/minjay/div_curl'))
+addpath(genpath('/home/minjay/model_output'))
+addpath(genpath('/home/minjay/nonsta_matern'))
+addpath(genpath('/home/minjay/bspline'))
+
 parpool(20)
 
 load('Y_Gau_Matern_bootstrap.mat')
